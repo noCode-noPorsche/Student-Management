49f42721dcfd4fed9689de29ee13e63145a9c1ff
Tsan Tri Duc <tsantriduc@gmail.com>
2024-11-17 16:40:15 +0700

    set up pagination and improve UI, caching React Query

--- src/pages/Students/Students.tsx
+++ src/pages/Students/Students.tsx
@@ -1,9 +1,12 @@
-import { useQuery } from '@tanstack/react-query'
+import { keepPreviousData, useQuery } from '@tanstack/react-query'
 import { getStudents } from 'apis/students.api'
+import classNames from 'classnames'
 import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Students as StudentsType } from 'types/students.type'
 import { useQueryString } from 'utils/utils'
+
+const LIMIT = 10
 
 export default function Students() {
   // const [students, setStudents] = useState<StudentsType>([])
@@ -22,15 +25,19 @@ export default function Students() {
   const queryString: { page?: string } = useQueryString()
   const page = Number(queryString.page) || 1
 
-  const { data, isLoading }= useQuery({
-    queryKey: ["students", page],
-    queryFn: () => getStudents(page, 10)
+  const { data, isLoading } = useQuery({
+    queryKey: ['students', page],
+    queryFn: () => getStudents(page, LIMIT),
+    placeholderData: keepPreviousData
   })
-  
+
+  const totalStudentsCount = Number(data?.headers['x-total-count']) || 0
+  const totalPage = Math.ceil(totalStudentsCount / LIMIT)
+
   return (
     <div>
       <h1 className='text-lg'>Students</h1>
-      {isLoading &&
+      {isLoading && (
         <div role='status' className='mt-6 animate-pulse'>
           <div className='mb-4 h-4  rounded bg-gray-200 dark:bg-gray-700' />
           <div className='mb-2.5 h-10  rounded bg-gray-200 dark:bg-gray-700' />
@@ -47,82 +54,116 @@ export default function Students() {
           <div className='h-10  rounded bg-gray-200 dark:bg-gray-700' />
           <span className='sr-only'>Loading...</span>
         </div>
-      }
-      {!isLoading && <>
-        <div className='relative mt-6 overflow-x-auto shadow-md sm:rounded-lg'>
-          <table className='w-full text-left text-sm text-gray-500 dark:text-gray-400'>
-            <thead className='bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400'>
-              <tr>
-                <th scope='col' className='py-3 px-6'>
-                  #
-                </th>
-                <th scope='col' className='py-3 px-6'>
-                  Avatar
-                </th>
-                <th scope='col' className='py-3 px-6'>
-                  Name
-                </th>
-                <th scope='col' className='py-3 px-6'>
-                  Email
-                </th>
-                <th scope='col' className='py-3 px-6'>
-                  <span className='sr-only'>Action</span>
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {data?.data.map((student) => (
-                <tr className='border-b bg-white hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-600'>
-                  <td className='py-4 px-6'>{student.id}</td>
-                  <td className='py-4 px-6'>
-                    <img src={student.avatar} alt={student.last_name} className='h-5 w-5' />
-                  </td>
-                  <th scope='row' className='whitespace-nowrap py-4 px-6 font-medium text-gray-900 dark:text-white'>
-                    {student.last_name}
+      )}
+      {!isLoading && (
+        <>
+          <div className='relative mt-6 overflow-x-auto shadow-md sm:rounded-lg'>
+            <table className='w-full text-left text-sm text-gray-500 dark:text-gray-400'>
+              <thead className='bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400'>
+                <tr>
+                  <th scope='col' className='py-3 px-6'>
+                    Id
                   </th>
-                  <td className='py-4 px-6'>{student.email}</td>
-                  <td className='py-4 px-6 text-right'>
-                    <Link to='/students/1' className='mr-5 font-medium text-blue-600 hover:underline dark:text-blue-500'>
-                      Edit
-                    </Link>
-                    <button className='font-medium text-red-600 dark:text-red-500'>Delete</button>
-                  </td>
+                  <th scope='col' className='py-3 px-6'>
+                    Avatar
+                  </th>
+                  <th scope='col' className='py-3 px-6'>
+                    Name
+                  </th>
+                  <th scope='col' className='py-3 px-6'>
+                    Email
+                  </th>
+                  <th scope='col' className='py-3 px-6'>
+                    <span className='sr-only'>Action</span>
+                  </th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-
-        <div className='mt-6 flex justify-center'>
-          <nav aria-label='Page navigation example'>
-            <ul className='inline-flex -space-x-px'>
-              <li>
-                <span className='cursor-not-allowed rounded-l-lg border border-gray-300 bg-white py-2 px-3 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'>
-                  Previous
-                </span>
-              </li>
-              <li>
-                <a
-                  className='border border-gray-300 bg-white bg-white py-2 px-3 leading-tight text-gray-500 text-gray-500  hover:bg-gray-100 hover:bg-gray-100 hover:text-gray-700 hover:text-gray-700'
-                  href='/students?page=8'
-                >
-                  1
-                </a>
-              </li>
-              <li>
-                <a
-                  className='rounded-r-lg border border-gray-300 bg-white py-2 px-3 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'
-                  href='/students?page=1'
-                >
-                  Next
-                </a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </>}
-
-
+              </thead>
+              <tbody>
+                {data?.data.map((student) => (
+                  <tr
+                    key={student.id}
+                    className='border-b bg-white hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-600'
+                  >
+                    <td className='py-4 px-6'>{student.id}</td>
+                    <td className='py-4 px-6'>
+                      <img src={student.avatar} alt={student.last_name} className='h-5 w-5' />
+                    </td>
+                    <th scope='row' className='whitespace-nowrap py-4 px-6 font-medium text-gray-900 dark:text-white'>
+                      {student.last_name}
+                    </th>
+                    <td className='py-4 px-6'>{student.email}</td>
+                    <td className='py-4 px-6 text-right'>
+                      <Link
+                        to='/students/1'
+                        className='mr-5 font-medium text-blue-600 hover:underline dark:text-blue-500'
+                      >
+                        Edit
+                      </Link>
+                      <button className='font-medium text-red-600 dark:text-red-500'>Delete</button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          <div className='mt-6 flex justify-center'>
+            <nav aria-label='Page navigation example'>
+              <ul className='inline-flex -space-x-px'>
+                <li>
+                  {page === 1 ? (
+                    <span className='cursor-not-allowed rounded-l-lg border border-gray-300 bg-white py-2 px-3 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'>
+                      Previous
+                    </span>
+                  ) : (
+                    <Link
+                      className='rounded-l-lg border border-gray-300 bg-white py-2 px-3 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'
+                      to={`/students?page=${page - 1}`}
+                    >
+                      Previous
+                    </Link>
+                  )}
+                </li>
+                {Array(totalPage)
+                  .fill(0)
+                  .map((_, index) => {
+                    const pageNumber = index + 1
+                    const isActive = page === pageNumber
+                    return (
+                      <li key={pageNumber}>
+                        <Link
+                          className={classNames(
+                            'border border-gray-300 py-2 px-3 leading-tigh  hover:bg-gray-100 hover:text-gray-700',
+                            {
+                              'bg-gray-100 text-gray-700': isActive,
+                              'bg-white text-gray-500': !isActive
+                            }
+                          )}
+                          to={`/students?page=${pageNumber}`}
+                        >
+                          {pageNumber}
+                        </Link>
+                      </li>
+                    )
+                  })}
+                <li>
+                  {page === totalPage ? (
+                    <span className=' cursor-not-allowed rounded-r-lg border border-gray-300 bg-white py-2 px-3 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'>
+                      Next
+                    </span>
+                  ) : (
+                    <Link
+                      className='rounded-r-lg border border-gray-300 bg-white py-2 px-3 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'
+                      to={`/students?page=${page + 1}`}
+                    >
+                      Next
+                    </Link>
+                  )}
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </>
+      )}
     </div>
   )
 }
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,7 +1,7 @@
-import { useSearchParams } from "react-router-dom"
+import { useSearchParams } from 'react-router-dom'
 
 export const useQueryString = () => {
-    const [searchParams] = useSearchParams()
-    const searchParamsObject = Object.fromEntries([...searchParams])
-    return searchParamsObject
+  const [searchParams] = useSearchParams()
+  const searchParamsObject = Object.fromEntries([...searchParams])
+  return searchParamsObject
 }
